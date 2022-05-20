fca7af232b795d3d3febeafad1b58c4de420bc89
SterlingB9 <SBoelter727@gmail.com>
2022-05-20 13:56:00 -0600

    team04

--- src/js/product.js
+++ src/js/product.js
@@ -2,39 +2,9 @@ import ProductData from "./productData.js";
 import ProductDetails from './productDetails.js';
 import { getParams } from "./utils.js";
 
+console.log("hello");
 const productId = getParams("product");
 const dataSource = new ProductData('tents');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-console.log(dataSource.findProductById(productId));
-
-let products = [];
-
-function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-// or should we do it this way?
-// async function getProductsDataAwait() {
-//   products = await fetch("../json/tents.json").then(convertToJson);
-// }
-
-// add to cart button event handler
-function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  setLocalStorage("so-cart", product);
-}
-
-// add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCart);
-
-
-
-
-
-
-
-
-
