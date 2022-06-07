1a4e01fcf37a22138a9f15b2ae2fd52bf0a2b612
= <andresfm@byui.edu>
2022-06-07 12:21:18 -0600

    pull before checkout

--- src/js/checkoutProcess.js
+++ src/js/checkoutProcess.js
@@ -1,28 +1,23 @@
 export default class checkoutProcess {
-    constructor (key, outputSelector){
-        this.key = key;
-        this.outputSelector = outputSelector;
-        this.list = [];
-        this.itemTotal = 0;
-        this.shipping = 0;
-        this.tax = 0;
-        this.orderTotal = 0;
+  constructor(key, outputSelector) {
+    this.key = key;
+    this.outputSelector = outputSelector;
+    this.list = [];
+    this.itemTotal = 0;
+    this.shipping = 0;
+    this.tax = 0;
+    this.orderTotal = 0;
+  }
 
-    }
+  init() {
+    this.list = getLocalStorage(this.key);
+    this.calculateItemSummary();
+  }
+  calculateItemSummary() {}
 
-    init(){
-        this.list = getLocalStorage(this.key);
-        this.calculateItemSummary();
-    }
-    calculateItemSummary(){
+  calculateOrderTotal() {
+    this.displayOrderTotals();
+  }
 
-    }
-
-    calculateOrderTotal(){
-        this.displayOrderTotals();
-    }
-
-    displayOrderTotals(){
-        
-    }
+  displayOrderTotals() {}
 }
